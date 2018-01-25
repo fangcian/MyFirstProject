d7c27bb5ead07b45be900daf5e4f26ce0a0b61f5
fangcian <kellymaple1@gmail.com>
2018-01-25 17:18:17 +0800

    Init project

---
+++ app/src/main/java/com/fangcian/myfirstproject/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.fangcian.myfirstproject
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.fangc0000ian.myfirstproject.HomeFragment
+
+class MainActivity : AppCompatActivity() {
+
+    private var mIsLogin = false
+    lateinit var test: SharedPreferences
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        test = getSharedPreferences("test", Context.MODE_PRIVATE)
+        mIsLogin = test.getBoolean("IsLogin", false)
+
+        if (mIsLogin) {
+            supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.fl_main_container, HomeFragment(), "HomeFragment")
+                    .commit()
+        } else {
+            supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.fl_main_container, LoginFragment(), "LoginFragment")
+                    .commit()
+        }
+
+
+    }
+}
+
